1a87618a0c2c0e1029a3aa97af9244deb0687818
heKim <khu4237@gmail.com>
2024-07-24 17:27:05 +0900

    feat : 메인 페이지 레이아웃 구성
    🔨 ADD
        - 메인페이지 레이아웃 구성, 버튼 연결
    🩹 FIX
        - mypagepage-> myinfopage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Main from '../components/Main'
 
 function MainPage() {
     return (
       <div className="main-page">
+        <Main />
       </div>
     );
   }
