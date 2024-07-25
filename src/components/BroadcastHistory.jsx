63dfbec53a6a8c6bf1b130a4d92810214c32074a
chamcham0707 <cheamin6306@naver.com>
2024-07-25 19:27:40 +0900

    ✨ feature: 주문내역 상세 페이지 구현

--- src/components/BroadcastHistory.jsx
+++ src/components/BroadcastHistory.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/BroadcastHistory.module.css';
+import styles from '../styles/History.module.css';
 
 const BroadcastHistory = () => {
   return (
