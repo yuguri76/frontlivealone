d0557bb6e467de4b430f4131db0cea9bd6928050
kopite97 <tjrwns3428@gmail.com>
2024-07-26 11:12:52 +0900

    feat: 채팅 최적화 추가 (오래된 메시지 삭제)

--- src/components/chat/ChatBox.jsx
+++ src/components/chat/ChatBox.jsx
@@ -6,6 +6,7 @@ function ChatContainer() {
   const chatWindowRef = useRef();
   const socket = useRef();
   const [messages, setMessages] = useState([]);
+  const MAX_MESSAGES = 100;
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:8080/ws');
@@ -20,7 +21,14 @@ function ChatContainer() {
       const [userNickname, newMessage] = data.split(/:(.+)/, 2);
 
       // 이전 상태에 새 값을 추가하여 상태 업데이트
-      setMessages((prevMessages) => [...prevMessages, { nickname: userNickname, text: newMessage }]);
+      setMessages((prevMessages) => {
+        // 메시지가 최대치를 초과하면 오래된 메시지 삭제
+        const updatedMessages = [...prevMessages, { nickname: userNickname, text: newMessage }];
+        if (updatedMessages.length > MAX_MESSAGES) {
+          updatedMessages.shift(); // 배열의 첫 번째 요소 제거
+        }
+        return updatedMessages;
+      });
     };
 
     return () => {
@@ -31,6 +39,9 @@ function ChatContainer() {
   useEffect(() => {
     if (chatWindowRef.current) {
       chatWindowRef.current.scrollTop = chatWindowRef.current.scrollHeight;
+    }
+    if(messages.length>20){
+      
     }
   }, [messages]); // messages 상태가 업데이트될 때마다 실행
 
@@ -75,7 +86,7 @@ function ChatList({messages}){
       height={800}
       itemCount={messages.length}
       itemSize={35}
-      width={400}
+      width={380}
     >
       {({ index, style }) => (
         <div style={style}>
