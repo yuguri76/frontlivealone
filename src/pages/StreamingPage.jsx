b3e12fdb0987a24473e0ee8ba0088b0947eccb3c
ggumi <tndus50843@gmail.com>
2024-07-29 11:15:06 +0900

    feat: 스트리밍 비디오 플레이어 기능 추가
     ADD
        - video.js를 사용하여 비디오 플레이어 기능 추가

--- src/pages/StreamingPage.jsx
+++ src/pages/StreamingPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import HLSPlayer from '../components/HLSPlayer';
 
 function StreamingPage() {
 
 
   return (
-    <div className="streaming-page">
-    </div>
-    );
-  }
+      <div className="streaming-page">
+          <div>
+              <HLSPlayer src="http://localhost:8080/hls/test.m3u8" /> {/* HLS 스트림 URL 전달 */}
+          </div>
+      </div>
+  );
+}
 
 export default StreamingPage;
