e1f09ce3cd29f91db25f0870c8fb34b13fd59686
kopite97 <tjrwns3428@gmail.com>
2024-07-31 18:59:38 +0900

    feat: data.type 세분화, token재 인증 로직 추가

--- src/hooks/useWebSocket.jsx
+++ src/hooks/useWebSocket.jsx
@@ -6,16 +6,21 @@ const useWebSocket = (token) => {
     const [isAvailableChat, setAvailableChat] = useState(false);
     const [userNickname, setUserNickname] = useState('');
     const [streamKey, setStreamKey] = useState('');
+ 
     const MAX_MESSAGES = 100;
 
+
     useEffect(() => {
-        if (!token) return;
 
         socket.current = new WebSocket('ws://localhost:8080/ws');
 
         socket.current.onopen = () => {
             console.log('세션 연결 시도');
-            const authMessage = JSON.stringify({ type: 'AUTH', messenger: 'front-server', message: token });
+            const authMessage = JSON.stringify({ 
+                type: 'REQUEST_AUTH', 
+                messenger: 'front-server', 
+                message: token
+            });
             socket.current.send(authMessage);
         };
 
@@ -24,13 +29,54 @@ const useWebSocket = (token) => {
             console.log(data);
             const { type, messenger, message } = data;
 
-            if (type === 'AUTH') {
+            if (type === 'RESPONSE_AUTH') {
                 setUserNickname(messenger);
                 setAvailableChat(true);
 
-                const requestInit = JSON.stringify({ type: 'INIT', messenger: messenger, message: 'Request Initialize' });
+                const requestInit = JSON.stringify({ 
+                    type: 'REQUEST_CHAT_INIT', 
+                    messenger: messenger, 
+                    message: 'Request Initialize' 
+                });
+
                 socket.current.send(requestInit);
-            } else if (type === 'MESSAGE') {
+            }else if(type==='INVALID_TOKEN'){ // 유효하지 않은 토큰 -> 리프레쉬 요청
+                const refreshToken = localStorage.getItem('refreshToken');
+            
+                const requestRefresh = JSON.stringify({
+                    type:'REQUEST_REFRESH',
+                    messenger:"front-server",
+                    message:refreshToken
+                });
+
+                socket.current.send(requestRefresh);
+            } 
+            else if(type==='RESPONSE_REFRESH'){ // Refresh 성공 (refresh가 유효할 경우)
+                const {access,refresh} = JSON.parse(message);
+             
+                localStorage.setItem('accessToken',access);
+                localStorage.setItem('refreshToken',refresh);
+                // Refresh성공으니 다시 AUTH보내기 
+                const authMessage = JSON.stringify({ 
+                    type: 'REQUEST_AUTH', 
+                    messenger: 'front-server', 
+                    message: access
+                });
+                socket.current.send(authMessage);
+                
+            }else if(type==='ANONYMOUS_USER'){ // Refresh도 성공 못하면 익명 유저
+                setAvailableChat(false);
+                
+                // 일단 채팅창은 가능 하도록
+                const requestInit = JSON.stringify({ 
+                    type: 'REQUEST_CHAT_INIT', 
+                    messenger: messenger, 
+                    message: 'Request Initialize' 
+                });
+
+                socket.current.send(requestInit);
+            }
+            else if (type === 'CHAT_MESSAGE') {
                 setMessages((prevMessages) => {
                     const updatedMessages = [...prevMessages, { nickname: messenger, text: message }];
                     if (updatedMessages.length > MAX_MESSAGES) {
@@ -42,7 +88,7 @@ const useWebSocket = (token) => {
                 const messageInput = document.getElementById('messageInput');
                 messageInput.placeholder = '채팅 입력 불가';
                 setAvailableChat(false);
-            } else if (type === 'INIT') {
+            } else if (type === 'RESPONSE_CHAT_INIT') {
                 console.log('init메시지',message);
                 const initMessages = JSON.parse(message).map(({ init_nickname, init_text }) => ({
                     nickname: init_nickname,
@@ -69,7 +115,7 @@ const useWebSocket = (token) => {
     const sendMessage = (userNickname, message) => {
         if (socket.current && isAvailableChat) {
             socket.current.send(JSON.stringify({
-                type: 'MESSAGE',
+                type: 'CHAT_MESSAGE',
                 messenger: userNickname,
                 message: message
             }));
