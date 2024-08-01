0ace69b3a897c3cfbde63340ac602dc58867f77b
heKim <khu4237@gmail.com>
2024-08-01 17:25:37 +0900

    feat: 스트림 키 웹소켓으로 🔨 ADD     - 스트림 키 http 말고 웹소켓으로 가져옴

--- src/components/LiveScreen.jsx
+++ src/components/LiveScreen.jsx
@@ -1,36 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import HLSPlayer from '../components/HLSPlayer';
-import axiosInstance from '../axiosInstance';
 import useWebSocket from '../hooks/useWebSocket';
 import loginLogo from '../assets/images/login_logo.png'
 
 const LiveScreen = () => {
-    const { wsStreamKey } = useWebSocket('');
-    const [streamKey, setStreamKey] = useState('');
-    const [isReady, setIsReady] = useState(false);
+    const { requestStreamKey, wsIsLive, wsStreamKey } = useWebSocket('');
 
     useEffect(() => {
-        const getStreamKey = async () => {
-            try {
-                const response = await axiosInstance.get('/broadcast/streamKey', {});
-                setStreamKey(response.data.data.stream_key);
-                setIsReady(true);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        getStreamKey();
-    }, []);
-
-    useEffect(() => {
-        setStreamKey(wsStreamKey);
-    }, [wsStreamKey]);
+        requestStreamKey();
+    }, [requestStreamKey]);
 
     return (
         <div>
-            {isReady ? (
-                <HLSPlayer src={`http://localhost:8081/hls/${streamKey}.m3u8`} />
+            {wsIsLive ? (
+                <HLSPlayer src={`http://localhost:8081/hls/${wsStreamKey}.m3u8`} />
             ) : (
                 <img src={loginLogo} alt="logo"
                     style={{ width: '300px', margin: '10px' }} />
