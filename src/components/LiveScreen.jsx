3f2097b1131916bd1b9183bd49059519b8809a25
heKim <khu4237@gmail.com>
2024-08-01 12:46:23 +0900

    feat: 스트림 키 가져오기
    🔨 ADD
        - 스트림 키 가져오기
        - 라이브 화면 컴포넌트 화

--- src/components/LiveScreen.jsx
+++ src/components/LiveScreen.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import styles from '../styles/LiveScreen.module.css';
+import React, { useEffect, useState } from 'react';
+import HLSPlayer from '../components/HLSPlayer';
+import axiosInstance from '../axiosInstance';
+import useWebSocket from '../hooks/useWebSocket';
+import loginLogo from '../assets/images/login_logo.png'
 
 const LiveScreen = () => {
+    const { wsStreamKey } = useWebSocket('');
+    const [streamKey, setStreamKey] = useState('');
+    const [isReady, setIsReady] = useState(false);
+
+    useEffect(() => {
+        const getStreamKey = async () => {
+            try {
+                const response = await axiosInstance.get('/broadcast/streamKey', {});
+                setStreamKey(response.data.data.stream_key);
+                setIsReady(true);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getStreamKey();
+    }, []);
+
+    useEffect(() => {
+        setStreamKey(wsStreamKey);
+    }, [wsStreamKey]);
+
     return (
-        <div className={styles.livescreen}>
+        <div>
+            {isReady ? (
+                <HLSPlayer src={`http://localhost:8081/hls/${streamKey}.m3u8`} />
+            ) : (
+                <img src={loginLogo} alt="logo"
+                    style={{ width: '300px', margin: '10px' }} />
+            )}
         </div>
     );
 };
