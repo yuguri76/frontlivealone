12c4f8a26d3ff4a858f4eb3c3f9153067a83ad54
ggumi <tndus50843@gmail.com>
2024-08-02 19:26:09 +0900

    fix : endpoint 변경
     Fix
        - endpoint 변경

--- src/components/LiveScreen.jsx
+++ src/components/LiveScreen.jsx
@@ -13,7 +13,7 @@ const LiveScreen = () => {
     return (
         <div>
             {wsIsLive ? (
-                <HLSPlayer src={`http://localhost:8081/hls/${wsStreamKey}.m3u8`} />
+                <HLSPlayer src={`http://ec2-52-79-50-124.ap-northeast-2.compute.amazonaws.com:8081/hls/${wsStreamKey}.m3u8`} />
             ) : (
                 <img src={logo} alt="logo"
                     style={{ width: '730px'}} />
