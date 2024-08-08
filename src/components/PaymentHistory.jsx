755c1685c0c9875048ef13eba721a0761cde0403
gyul <ysy9976@naver.com>
2024-08-09 04:56:07 +0900

    fix : 주문내역 조회 버그 수정
     FIX
        - MyInfo.jsx
    
    BREAKING CHANGE(중요한 변경 사항):

--- src/components/PaymentHistory.jsx
+++ src/components/PaymentHistory.jsx
@@ -7,29 +7,10 @@ import classNames from "classnames";
 const PaymentHistory = () => {
   const location = useLocation();
   const {payments} = location.state;
+  const [cursor, setCursor] = useState(0);
 
-  const [selectPageOne, setSelectPageOne] = useState(true);
-  const [selectPageTwo, setSelectPageTwo] = useState(false);
-  const [selectPageThree, setSelectPageThree] = useState(false);
-  const [selectPageFour, setSelectPageFour] = useState(false);
-  const [selectPageFive, setSelectPageFive] = useState(false);
-
-  const handlePageClick = async (event) => {
-    let page = parseInt(event.target.textContent);
-
-    setSelectPageOne(false);
-    setSelectPageTwo(false);
-    setSelectPageThree(false);
-    setSelectPageFour(false);
-    setSelectPageFive(false);
-
-    switch (page) {
-      case 1: setSelectPageOne(true); break;
-      case 2: setSelectPageTwo(true); break;
-      case 3: setSelectPageThree(true); break;
-      case 4: setSelectPageFour(true); break;
-      case 5: setSelectPageFive(true); break;
-    }
+  const handleClick = cursor => event => {
+    setCursor(cursor)
   }
 
   return (
@@ -40,7 +21,7 @@ const PaymentHistory = () => {
         <div className={styles.line}></div>
         <div className={styles.historyContentContainer}>
           {
-            payments.map((content, index) => {
+            payments.slice(cursor * 5, cursor * 5 + 5).map((content, index) => {
               const {created_at} = content;
               const [createdDate, rawCreatedTime] = created_at.split('T') // ['YYYY-MM-DD', 'HH:MM:SS.ms']
               const [createdTime, _] = rawCreatedTime.split('.');
@@ -58,16 +39,16 @@ const PaymentHistory = () => {
           }
         </div>
         <div className={styles.pageContainer}>
-          <span className={classNames({[styles.pageSelect]: selectPageOne})}
-                onClick={handlePageClick}>1</span>
-          <span className={classNames({[styles.pageSelect]: selectPageTwo})}
-                onClick={handlePageClick}>2</span>
-          <span className={classNames({[styles.pageSelect]: selectPageThree})}
-                onClick={handlePageClick}>3</span>
-          <span className={classNames({[styles.pageSelect]: selectPageFour})}
-                onClick={handlePageClick}>4</span>
-          <span className={classNames({[styles.pageSelect]: selectPageFive})}
-                onClick={handlePageClick}>5</span>
+          <span className={classNames({[styles.pageSelect]: cursor === 0})}
+                onClick={handleClick(0)}>1</span>
+          <span className={classNames({[styles.pageSelect]: cursor === 1})}
+                onClick={handleClick(1)}>2</span>
+          <span className={classNames({[styles.pageSelect]: cursor === 2})}
+                onClick={handleClick(2)}>3</span>
+          <span className={classNames({[styles.pageSelect]: cursor === 3})}
+                onClick={handleClick(3)}>4</span>
+          <span className={classNames({[styles.pageSelect]: cursor === 4})}
+                onClick={handleClick(4)}>5</span>
         </div>
         >
       </div>
