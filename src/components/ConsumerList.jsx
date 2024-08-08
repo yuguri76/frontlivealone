52f50bf487fc70e7f1737a115c1bcaa39ea89981
chamcham0707 <cheamin6306@naver.com>
2024-08-08 16:21:44 +0900

    ✨ featurer: 서버에서 정보 받아와 보여주는 기능 구현

--- src/components/ConsumerList.jsx
+++ src/components/ConsumerList.jsx
@@ -5,23 +5,60 @@ import {useNavigate, useParams} from "react-router-dom";
 import axiosInstance from "../axiosInstance";
 
 const ConsumerList = () => {
+  const navigate = useNavigate();
+
   const { broadcastId: paramBroadcastId} = useParams();
   const [broadcastId, setBroadcastId] = useState(paramBroadcastId);
+  const [broadcastTitle, setBroadcastTitle] = useState('');
+  const [consumerList, setConsumerList] = useState([]);
+
+  const [totalPage, setTotalPage] = useState(1);
 
   useEffect(() => {
+    getBroadcastTitle();
     getConsumerList();
   }, []);
 
-  const getConsumerList = async () => {
+  const getBroadcastTitle = async () => {
+    try {
+      const response = await axiosInstance.get(
+          `/broadcast/${broadcastId}`, {}, {
+            headers: {
+              Authorization: localStorage.getItem('accessToken')
+            },
+          });
+
+      console.log(response.data.data);
+      setBroadcastTitle(response.data.data.title);
+    } catch (error) {
+      if (error.response.data.message) {
+        console.error(
+            `Get broadcast title error: ${error.response.data.message}`);
+      }
+    }
+  };
+
+  const getConsumerList = async (event) => {
+
+    let page = 1;
+    if (event && event.target) {
+      page = parseInt(event.target.innerHTML, 10);
+    }
+
     try {
       const response = await axiosInstance.get(
           `/admin/broadcast/${broadcastId}/consumer`, {}, {
             headers: {
               Authorization: localStorage.getItem('accessToken')
+            },
+            params: {
+              "page": page
             }
           });
 
       console.log(response.data.data);
+      setConsumerList(response.data.data.content);
+      setTotalPage(response.data.data.total_pages);
     } catch (error) {
       if (error.response.data.message) {
         console.error(
@@ -30,12 +67,24 @@ const ConsumerList = () => {
     }
   };
 
+  const handleConsumerClick = (event) => {
+    navigate(`/user/${event.currentTarget.dataset.id}`)
+  };
+
+  const handleAdminPageClick = () => {
+    navigate('/admin');
+  };
+
+  const handlerBroadcastTitleClick = () => {
+    navigate(`/admin/broadcast/${broadcastId}`);
+  };
+
   return (
       <div className={styles.adminConsumerContainer}>
         <div className={styles.adminMenuContainer}>
-          <span>관리자 페이지</span>
+          <span onClick={handleAdminPageClick}>관리자 페이지</span>
           <span>></span>
-          <span>선산곱창 할인</span>
+          <span onClick={handlerBroadcastTitleClick}>{broadcastTitle}</span>
           <span>></span>
           <span>구매자 목록</span>
         </div>
@@ -51,104 +100,27 @@ const ConsumerList = () => {
             </tr>
             </thead>
             <tbody>
-            <tr>
-              <td>1.</td>
-              <td>홍길동</td>
-              <td>10 개</td>
-              <td>90000 원</td>
-              <td>2023-23-23 23:12:12</td>
-            </tr>
-            <tr>
-              <td>1.</td>
-              <td>홍길동</td>
-              <td>10 개</td>
-              <td>90000 원</td>
-              <td>2023-23-23 23:12:12</td>
-            </tr>
-            <tr>
-              <td>1.</td>
-              <td>홍길동</td>
-              <td>10 개</td>
-              <td>90000 원</td>
-              <td>2023-23-23 23:12:12</td>
-            </tr>
-            <tr>
-              <td>1.</td>
-              <td>홍길동</td>
-              <td>10 개</td>
-              <td>90000 원</td>
-              <td>2023-23-23 23:12:12</td>
-            </tr>
-            <tr>
-              <td>1.</td>
-              <td>홍길동</td>
-              <td>10 개</td>
-              <td>90000 원</td>
-              <td>2023-23-23 23:12:12</td>
-            </tr>
-            <tr>
-              <td>1.</td>
-              <td>홍길동</td>
-              <td>10 개</td>
-              <td>90000 원</td>
-              <td>2023-23-23 23:12:12</td>
-            </tr>
-            <tr>
-              <td>1.</td>
-              <td>홍길동</td>
-              <td>10 개</td>
-              <td>90000 원</td>
-              <td>2023-23-23 23:12:12</td>
-            </tr>
-            <tr>
-              <td>1.</td>
-              <td>홍길동</td>
-              <td>10 개</td>
-              <td>90000 원</td>
-              <td>2023-23-23 23:12:12</td>
-            </tr>
-            <tr>
-              <td>1.</td>
-              <td>홍길동</td>
-              <td>10 개</td>
-              <td>90000 원</td>
-              <td>2023-23-23 23:12:12</td>
-            </tr>
-            <tr>
-              <td>1.</td>
-              <td>홍길동</td>
-              <td>10 개</td>
-              <td>90000 원</td>
-              <td>2023-23-23 23:12:12</td>
-            </tr>
+            {consumerList.map((consumer, index) => (
+                <tr key={index}>
+                  <td>{index + 1}.</td>
+                  <td data-id={consumer.user_id} onClick={handleConsumerClick} className={styles.consumerName}>{consumer.username}</td>
+                  <td>{consumer.product_quantity} 개</td>
+                  <td>{consumer.payment_amount} 원</td>
+                  <td>{(consumer.order_date).replace('T', ' ')}</td>
+                </tr>
+            ))}
             </tbody>
           </table>
         </div>
         <div className={styles.consumerListPageContainer}>
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>4</span>
-          <span>5</span>
+          {Array.from({length: totalPage}, (_, index) => (
+              <span key={index} onClick={getConsumerList}>
+                {index + 1}
+              </span>
+          ))}
         </div>
       </div>
   );
 };
 
 export default ConsumerList;
-
-// <div className={styles.consumerMenu}>
-//   <span>이름</span>
-//   <span>구매 개수</span>
-//   <span>결제 금액</span>
-//   <span>주문 일시</span>
-// </div>
-// <div className={styles.consumersContainer}>
-//   <div className={styles.consumerContainer}>
-//     <span>1. </span>
-//     <span>홍길동</span>
-//     <span>10 개</span>
-//     <span>90000 원</span>
-//     <span>2020-30-93 23:23:12</span>
-//   </div>
-// </div>
