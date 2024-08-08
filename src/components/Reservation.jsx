d19bfabb1009c2fbb1f12d85fce8919160caf776
heKim <khu4237@gmail.com>
2024-08-08 15:47:28 +0900

    feat : 방송 예약 페이지 개발
    ADD
      - 오늘~일주일 뒤 예약 가능
      - 예약 하면 메일 옴

---
+++ src/components/Reservation.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import styles from '../styles/Reservation.module.css';
+import axiosInstance from '../axiosInstance';
+
+const Reservation = () => {
+    const currentDate = new Date();
+    const maxDate = new Date().setDate(currentDate.getDate() + 6);
+    const [selectedDate, setSelectedDate] = useState(currentDate);
+    const [timeslots, setTimeslots] = useState([]);
+
+    useEffect(() => {
+        getReservations(selectedDate);
+    }, []);
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+        getReservations(date);
+    };
+
+    const getReservations = async (date) => {
+        try {
+            const response = await axiosInstance.get('/broadcast/reservations', {
+                params: {
+                    date: date.toISOString().split('T')[0]
+                }
+            });
+            setTimeslots(response.data.data);
+        } catch (error) {
+            setTimeslots([]);
+        }
+    };
+
+    const handleReservation = async (slot) => {
+        if (slot.reserved) {
+            return;
+        }
+
+        try {
+            const response = await axiosInstance.post('/broadcast/reservation',
+                {
+                    airtime: `${selectedDate.toISOString().split('T')[0]}T${slot.time}`
+                }, {
+                headers: {
+                    Authorization: localStorage.getItem('accessToken')
+                }
+            }
+            );
+
+            alert(response.data.message);
+
+        } catch (error) {
+            if (error.response.data.message) {
+                alert(`${error.response.data.message}`);
+            }
+        }
+        getReservations(selectedDate);
+    };
+
+    const formatTime = (time) => {
+        const [hours, minutes] = time.split(':');
+
+        return `${hours}시 ${minutes}분`;
+    };
+
+    return (
+        <div className={styles.reservationContainer}>
+            <div className={styles.calendar}>
+                <DatePicker
+                    selected={selectedDate}
+                    onChange={handleDateChange}
+                    minDate={currentDate}
+                    maxDate={maxDate}
+                    inline
+                    dateFormat="yyyy-MM-dd"
+                />
+            </div>
+            <div className={styles.timeslots}>
+                {timeslots.map((slot, index) => (
+                    <div key={index} className={styles.timeslot}>
+                        <span>{formatTime(slot.time)}</span>
+                        <button className={slot.reserved ? styles.unavailable : styles.available} onClick={() => handleReservation(slot)}>
+                            {slot.reserved === true ? '마감' : '예약'}
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Reservation;
