123486324e556fd809140950d97aafd66f0b76db
gyul <ysy9976@naver.com>
2024-08-12 16:16:46 +0900

    feat : main 페이지 css 수정
    ✨ 내용 추가할 거 적어주세요
    
    PaymentPage, CompletePaymentPage css 구현 완료
    
     FIX
        - CompletePaymentPage.module.css
        - PaymentPage.module.css
    
    BREAKING CHANGE(중요한 변경 사항):

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,8 @@ import { useDispatch } from 'react-redux';
 import {
   setBroadcastId,
   setProductId,
-  setProductName, setProductPrice,
+  setProductName,
+  setProductPrice,
   setProductQuantity
 } from "../store/store";
 
@@ -62,7 +63,7 @@ const Main = () => {
       dispatch(setProductPrice(response.data.data.product_price));
     } catch (error) {
       console.error('상품, 방송 정보 가져오기 실패: ' + (error.response?.data?.message
-        || error.message));
+          || error.message));
     }
   };
 
@@ -71,33 +72,31 @@ const Main = () => {
   }
 
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.siteIntro}>
-        <h3>나 혼자 살고(Live) 나 혼자 산다(Buy)!</h3>
-        <h3>자취생들을 위한 홈쇼핑</h3>
-        <h3>나 혼자 산다에 오신 것을 환영합니다.</h3>
-        <p>나 혼자 산다는 자취생 꿀템을 판매하고 구매할 수 있는 라이브 커머스 사이트입니다.</p>
-      </div>
-      <div className={styles.sections}>
-        <div className={styles.streamerSection}>
-          <div className={styles.reservationPage}>
-            <div className={styles.streamerText}>예약 페이지</div>
-            <button className={styles.reservationButton} onClick={handleReservationClick}>예약하기</button>
+      <div className={styles.mainContainer}>
+        <div className={styles.siteIntro}>
+          <h3>나 혼자 살고(Live) 나 혼자 산다(Buy)!</h3>
+          <h3>자취생들을 위한 홈쇼핑</h3>
+          <h3>나 혼자 산다에 오신 것을 환영합니다.</h3>
+          <p>나 혼자 산다는 자취생 꿀템을 판매하고 구매할 수 있는 라이브 커머스 사이트입니다.</p>
+        </div>
+        <div className={styles.sections}>
+          <div className={styles.quickServices}>
+            <div className={styles.quickServiceButton} onClick={handleReservationClick}>
+              예약하기
+            </div>
+            <div className={styles.quickServiceButton} onClick={handleStreamerClick}>
+              방송하기
+            </div>
           </div>
-          <div className={styles.streamerPage}>
-            <div className={styles.streamerText}>스트리머 페이지</div>
-            <button className={styles.streamButton} onClick={handleStreamerClick}>방송하기</button>
+          <div className={styles.onAir}>
+            <div className={styles.onAirText}>ON AIR!</div>
+            <div className={styles.discountText}>{broadcastTitle}</div>
+            <button className={styles.watchButton} onClick={handleStreamingClick}>보러가기</button>
+          </div>
+          <div className={styles.ad} onClick={handleAdClick}>
           </div>
         </div>
-        <div className={styles.onAir}>
-          <div className={styles.onAirText}>ON AIR!</div>
-          <div className={styles.discountText}>{broadcastTitle}</div>
-          <button className={styles.watchButton} onClick={handleStreamingClick}>보러가기</button>
-        </div>
-        <div className={styles.ad} onClick={handleAdClick}>
-        </div>
       </div>
-    </div>
   );
 };
 
