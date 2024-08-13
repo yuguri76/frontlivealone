c25e3e77e25a772a7b7ef83a1268f5946aa05ff7
heKim <khu4237@gmail.com>
2024-08-13 16:45:32 +0900

    💄 style: 경고페이지, 예약페이지 css 수정

--- src/pages/WarningPage.jsx
+++ src/pages/WarningPage.jsx
@@ -5,8 +5,12 @@ import warning from '../assets/images/warning.png'
 function WarningPage() {
     return (
         <div className={styles.container}>
-            <img src={warning} alt="warning icon" className={styles.logo} />
+            <img src={warning} alt="warning icon" className={styles.icon} />
             <p className={styles.message}>페이지가 존재하지 않습니다.</p>
+            <p className={styles.subMessage}>
+                해당 페이지는 경로가 변경되었거나, 서버에 존재하지 않습니다.<br/>
+                파일의 경로가 정확한지, URL 주소를 바르게 입력했는지 확인하세요.
+            </p>
         </div>
     );
 }
