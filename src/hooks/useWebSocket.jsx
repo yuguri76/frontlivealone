e3dc5cb465d555332e20a646391e2ec1056d3b25
kopite97 <tjrwns3428@gmail.com>
2024-08-14 15:51:11 +0900

    feat:시청자 수 표시 방법 변경 -> 새로운 세션이 들어올 때마다

--- src/hooks/useWebSocket.jsx
+++ src/hooks/useWebSocket.jsx
@@ -45,6 +45,11 @@ const useWebSocket = (token,refreshToken) => {
                     const data = JSON.parse(message.body);
                     handleMessage(data);
                 });
+
+                client.current.subscribe('/queue/viewer', (message) => {
+                    const data = JSON.parse(message.body);
+                    handleViewerCount(data);
+                })
                 
                 client.current.subscribe('/user/queue/reply', (message) => {
                     const data = JSON.parse(message.body);
@@ -108,29 +113,29 @@ const useWebSocket = (token,refreshToken) => {
         };
     }, []);
 
-    useEffect(()=>{
+    // useEffect(()=>{
 
-        /**
-         * 10초에 한번 시청자 수 요청
-         * 시청자 수가 바뀔 때 마다 갱신도 가능 (적용은 x) -> 서버에서도 처리해야함 
-         */
-        const getViewerCountInterval = setInterval(()=>{
-            requestViewerCount();
-        },10000); 
+    //     /**
+    //      * 10초에 한번 시청자 수 요청
+    //      * 시청자 수가 바뀔 때 마다 갱신도 가능 (적용은 x) -> 서버에서도 처리해야함 
+    //      */
+    //     const getViewerCountInterval = setInterval(()=>{
+    //         requestViewerCount();
+    //     },10000); 
 
-        return () => clearInterval(getViewerCountInterval);
+    //     return () => clearInterval(getViewerCountInterval);
 
-    },[]);
+    // },[]);
 
-    const requestViewerCount = () =>{
-        console.log('시청자 수 요청');
-        const requestViewerCountMessage = JSON.stringify({
-            type: 'REQUEST_VIEWERCOUNT',
-            messenger: 'front-server',
-            message: 1
-        })
-        client.current.publish({destination: '/pub/session',body: requestViewerCountMessage})
-    };
+    // const requestViewerCount = () =>{
+    //     console.log('시청자 수 요청');
+    //     const requestViewerCountMessage = JSON.stringify({
+    //         type: 'REQUEST_VIEWERCOUNT',
+    //         messenger: 'front-server',
+    //         message: 1
+    //     })
+    //     client.current.publish({destination: '/pub/session',body: requestViewerCountMessage})
+    // };
 
     const handleMessage = (data) =>{
         const { type, messenger, message } = data;
@@ -144,6 +149,14 @@ const useWebSocket = (token,refreshToken) => {
                 }
                 return updatedMessages;
             });
+        }
+    }
+
+    const handleViewerCount = (data) =>{
+        const {type,messenger,message} = data;
+        console.log(data);
+        if (type === 'RESPONSE_VIEWERCOUNT'){
+            setViewrCount(message);
         }
     }
 
@@ -241,9 +254,7 @@ const useWebSocket = (token,refreshToken) => {
                 }
                 return updatedMessages;
             });
-        } else if (type === 'RESPONSE_VIEWERCOUNT'){
-            setViewrCount(message);
-        }
+        } 
     };
 
     const sendMessage = (userNickname, message) => {
